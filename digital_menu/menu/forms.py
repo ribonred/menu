5e1daeebb3f63c57f6217f3fad9320a33fd1c1a6
tamhor <tamhor.hidayat@gmail.com>
2019-08-17 17:28:25 +0700

    plusminus

--- digital_menu/menu/forms.py
+++ digital_menu/menu/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import widgets
 
-pilih_jumlah = [(i, str(i)) for i in range(1,21)]
+pilih_jumlah = [(i, str(i)) for i in range(1,11)]
 
 class formtambahproduk(forms.Form):
     quantity = forms.TypedMultipleChoiceField(choices=pilih_jumlah,coerce=int)
