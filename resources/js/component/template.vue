89729b100eada87ddd6c8d01b427c44628f3020b
agam-w <agam.w11@gmail.com>
2021-01-17 16:15:14 +0700

    add vuex

--- resources/js/component/template.vue
+++ resources/js/component/template.vue
@@ -2,27 +2,40 @@
 <div>
   <h1>{{ message }}</h1>
   <h2>{{ backend[0].email }}</h2>
-  
+  <button>change text</button>
 </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
     props: {
-
-    mess: {
-      type: String,
+      mess: {
+        type: String,
+      },
     },
-
-  },
-  data() {
-    return {
-      message: "Welcome to Vue!",
-      backend: JSON.parse(this.mess)
-    };
-  },
-  mounted() {
-    
-  }
+    data() {
+      return {
+        message: "Welcome to Vue!",
+        backend: JSON.parse(this.mess)
+      };
+    },
+    computed: mapState({
+      products: state => state.products.all
+    }),
+    watch: {
+      products (update, old) {
+        
+      }
+    },
+    methods: mapActions('cart', [
+      'addProductToCart'
+    ]),
+    created () {
+      this.$store.dispatch('products/getAllProducts')
+    },
+    mounted () {
+    }
 };
 </script>
--- resources/js/djmix.js
+++ resources/js/djmix.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import ExampleComponent from './component/template.vue';
+import store from './store'
 
 Vue.component('example-component', ExampleComponent);
 
 let vue = new Vue({
+    el: '#app',
+    store,
   //
 }).$mount('#app')
